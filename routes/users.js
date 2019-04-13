1aa8052ecda2cdec9d33468acbc46e4bf2a2b87c
dyyfk <36778268+dyyfk@users.noreply.github.com>
2019-04-13 12:49:27 -0500

    add mongodb route

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
-
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 
 router.get('/register', (req, res) => {
@@ -39,7 +39,33 @@ router.post('/register', (req, res) => {
             errors, name, email, password, password2
         });
     } else {
-        res.send('pass');
+        User.findOne({ email }).then(user => {
+            if (user) {
+                errors.push({ msg: 'Email has been registered' });
+                res.render('register', {
+                    errors, name, email, password, password2
+                });
+            } else {
+                const newUser = new User({
+                    errors, name, email, password, password2
+                });
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+
+                        newUser.password = hash;
+
+                        newUser.save().then(user => {
+                            res.redirect('/users/login');
+                        }).catch(err => console.log(err));
+
+                    });
+                });
+
+                res.send('Hello');
+            }
+        });
     }
 
 });

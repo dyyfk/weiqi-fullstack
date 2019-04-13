daf00910279fa36e01fea5afa18dae4815cf36ca
dyyfk <36778268+dyyfk@users.noreply.github.com>
2019-04-12 23:37:54 -0500

    add register routes

--- app.js
+++ app.js
@@ -2,13 +2,25 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const db = require('./config/keys').MongoURL;
 
+// MongoDB
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => {
+        console.log(`connected to MongoDB`);
+    }).catch(err => console.log(err));
+
+// BodyParser
+app.use(express.urlencoded({ extended: false }));
+
+// EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
+// Routes
 app.use('/', require('./routes/index'));
-
-app.use('/user', require('./routes/user'));
+app.use('/users', require('./routes/users'));
 
 
 app.listen(PORT, () => {
---
+++ routes/users.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+
+router.get('/register', (req, res) => {
+    res.render('register');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.post('/register', (req, res) => {
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+    // Check required fields
+
+    // TODO: use validation package here
+    if (!name || !email || !password || !password2) {
+        errors.push({ msg: 'Please fill in all fields' });
+    }
+
+    // check passwords match
+
+    if (password !== password2) {
+        errors.push({ msg: 'Passwords do not match' });
+    }
+
+    // check pass length
+    if (password.length < 6) {
+        errors.push({ msg: 'Password should be at least 6 characters' });
+    }
+
+
+    if (errors.length > 0) {
+        console.log(errors);
+        res.render('register', {
+            errors, name, email, password, password2
+        });
+    } else {
+        res.send('pass');
+    }
+
+});
+
+module.exports = router;

5eeb39fc32ecda0466ef99206df16a1812c4426d
dyyfk <36778268+dyyfk@users.noreply.github.com>
2019-04-13 14:19:05 -0500

    add file

---
+++ config/auth.js
@@ -0,0 +1,10 @@
+module.exports = {
+    ensureAuthenticated: function (req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        } else {
+            req.flash('error_msg', 'Please log in to view this page');
+            res.redirect('/users/login');
+        }
+    }
+};
---
+++ config/passport-setup.js
@@ -0,0 +1,38 @@
+const LocalStrategy = require('passport-local').Strategy;
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+
+const User = require('../models/User');
+
+module.exports = function (passport) {
+    passport.use(
+        new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+
+            User.findOne({ email }).then(user => {
+                if (!user) {
+                    return done(null, false, { message: 'That email is not registered' });
+                } else {
+                    bcrypt.compare(password, user.password, (err, isMatch) => {
+                        if (err) throw err;
+                        if (isMatch) {
+                            return done(null, user);
+                        } else {
+                            return done(null, false, { message: 'Password incorrect' });
+                        }
+                    });
+                }
+            }).catch(err => console.log(err));
+        })
+    );
+
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function (id, done) {
+        User.findById(id, function (err, user) {
+            done(err, user);
+        });
+    });
+};

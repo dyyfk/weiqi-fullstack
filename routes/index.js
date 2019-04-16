8543d0ef2a7773d52fab0846c9d73a7d689bdde7
dyyfk <36778268+dyyfk@users.noreply.github.com>
2019-04-16 01:36:25 -0500

    add login and logout toggle

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
 
 
-router.get('/', (req, res) => {
-    res.render('welcome');
+router.get('/', ensureAuthenticated, (req, res) => {
+    res.render('welcome', {
+        user: req.user
+    });
 });
 
 router.get('/dashboard', ensureAuthenticated, (req, res) => {

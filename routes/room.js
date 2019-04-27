0a040ed6ed84629435c41b8fb9963e183ab6695f
dyyfk <36778268+dyyfk@users.noreply.github.com>
2019-04-26 20:38:15 -0500

    add chessboard

--- routes/room.js
+++ routes/room.js
@@ -4,8 +4,9 @@ const { ensureAuthenticated } = require('../config/auth');
 const { isLoggedIn } = require('../config/auth');
 
 router.get('/', ensureAuthenticated, (req, res) => {
-    res.render('room', {
-        user: req.user
+    res.render('chessroom', {
+        user: req.user,
+        clean: true
     });
 });
 
